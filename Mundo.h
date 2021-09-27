7b80ef4a9775a31b3a1fd704bae4768729af6433
cesar madera <camgcamg11@gmail.com>
2021-09-27 18:41:14 -0500

    first refactoring

--- Mundo.h
+++ Mundo.h
@@ -4,10 +4,15 @@
 
 #ifndef RT1_MUNDO_H
 #define RT1_MUNDO_H
-
+#include "./Camara.h"
 
 class Mundo {
-
+  Camara cam;
+  Mundo(){
+    cam.calcularVectores(vec3f(-4, 10, 20), vec3f(0, 0, 0), vec3f(0, 1, 0));
+    cam.inicializar(800, 600, 45, 10);
+    cam.Renderizar();
+  }
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-#include "Camara.h"
+#include "Mundo.h"
 
 int main() {
-    Camara cam;
-    cam.calcularVectores(vec3f(-4,10,20), vec3f(0,0,0), vec3f(0,1,0));
-    cam.inicializar(800, 600, 45, 10);
-    cam.Renderizar();
-    return 0;
+	Mundo mundo();
+	return 0;
 }
